bdf223e0bd8bd8e72a635f3108fa144ec8cc4615
Minami <1243815594@qq.com>
2023-11-24 20:11:04 +0800

    清除

--- demo1/src/com/test/demotest.java
+++ demo1/src/com/test/demotest.java
@@ -5,11 +5,5 @@ public class demotest {
     public static void main(String[] args) {
         System.out.println("hello");
 
-        System.out.println("world");
-
-        System.out.println("out");
-        System.out.println("out");
-        for(int i=0;i<10;i++)
-            System.out.println(i);
     }
 }
